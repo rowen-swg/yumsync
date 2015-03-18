a8a7279b39527dfa7ad798fc65e8087c0d01f436
Vamegh Hedayati <vhedayati@local>
2015-03-18 22:57:22 +0000

    Changed stableversion to be an array.
    	modified:   pakrat/__init__.py

--- pakrat/__init__.py
+++ pakrat/__init__.py
@@ -7,7 +7,7 @@ from pakrat import util, log, repo, repos, progress
 
 __version__ = '0.3.2'
 
-def sync(basedir=None, objrepos=[], osvers=[], repoarches=[], stableversion=None, repodirs=[], repofiles=[],
+def sync(basedir=None, objrepos=[], osvers=[], repoarches=[], stableversion=[], repodirs=[], repofiles=[],
          repoversion=None, delete=False, combined=False, callback=None):
     """ Mirror repositories with configuration data from multiple sources.
 
@@ -40,12 +40,13 @@ def sync(basedir=None, objrepos=[], osvers=[], repoarches=[], stableversion=None
     for objrepo in objrepos:
         arch = repoarches.pop(0)
         osver = osvers.pop(0)
+        stable = stableversion.pop(0)
         prog.update(objrepo.id)  # Add the repo to the progress object
         yumcallback = progress.YumProgress(objrepo.id, queue, callback)
         repocallback = progress.ProgressCallback(queue, callback)
         dest = util.get_repo_dir(basedir, objrepo.id)
         p = multiprocessing.Process(target=repo.sync, args=(objrepo, dest, osver, arch,
-                                    repoversion, stableversion, delete, combined, yumcallback,
+                                    repoversion, stable, delete, combined, yumcallback,
                                     repocallback))
         p.start()
         processes.append(p)
