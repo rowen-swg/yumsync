e2528e21145e03e9a9a6ab838dc06e93785f3106
Romain Fihue <romain.fihue@cea.fr>
2019-12-12 09:11:42 +0100

    Enhance logging by using python-logging

--- yumsync/log.py
+++ yumsync/log.py
@@ -1,23 +1,11 @@
 import os
 import sys
 import time
+import logging
 
 start = int(time.time())
 
 def log(msg, header=False, log_dir=None, force=False):
-    time_str = time.strftime('%Y-%m-%dT%X%z')
-    delta = int(time.time()) - start
-    minute, second = divmod(delta, 60)
-    hour, minute = divmod(minute, 60)
-    delta_str = '{:02d}:{:02d}:{:02d}'.format(hour, minute, second)
-
     output_str = "==> %s" % msg if header else msg
 
-    if log_dir is not None:
-        if os.path.exists(log_dir):
-            with open(os.path.join(log_dir, 'sync.log'), 'a') as logfile:
-                logfile.write('{} {}\n'.format(time_str, output_str))
-
-    if force or not sys.__stdout__.isatty():
-        sys.__stdout__.write('{} {}\n'.format(delta_str, output_str))
-        sys.__stdout__.flush()
+    logging.info(output_str)
